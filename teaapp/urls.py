b4d9197091d8b73c538dd33f211fd38e93a717b5
Landon Morgan <morganlandon@gmail.com>
2020-05-15 15:11:48 -0500

    delete button added for teapackaging

--- teaapp/urls.py
+++ teaapp/urls.py
@@ -5,6 +5,7 @@ from .views import *
 app_name = "teaapp"
 urlpatterns = [
     path('', home, name='home'),
+    path('teas', home, name='home'),
     path('teas/form', tea_form, name='tea_form'),
     path('teas/<int:tea_id>/', tea_detail, name='tea_detail'),
 ]
